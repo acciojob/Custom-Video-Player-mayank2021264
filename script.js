f6a70933504c247212a15a38e514d90bf6a6e956
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-04-24 17:16:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+
+function playPause(){
+	if(video.paused{
+		video.play();
+		console.log("playing");
+	}
+	else{
+		video.pause();
+		console.log("paused");
+	}
+}
+function updateToggle() {
+	if(video.paused){
+		toggle.innerText = "►";
+	}
+	else{
+		toggle.innerText = "❚ ❚";
+	}
+}
+toggle.addEventListener('click', playPause);
+video.addEventListener('play', updateToggle);
+video.addEventListener('pause', updateToggle);
